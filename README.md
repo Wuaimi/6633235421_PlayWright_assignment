00840ce582372f67c697c53f1f8209ec01f76e1f
Wuaimi <10327sornpicha@gmail.com>
2026-02-28 01:26:14 +0700

    update a few transfer cases

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # 6633235421_PlayWright_assignment
 Practical software testing and quality Assurance week 7- PlayWright
 
-มีทั้งหมด 6 เคส 4 ไฟล์ อยู่ในโฟลเดอร์ Homework
+มีทั้งหมด 8 เคส 4 ไฟล์ อยู่ในโฟลเดอร์ Homework
 1. login success -> login.spec.js
 2. login fail -> login.spec.js
 3. deposit success -> deposit.spec.js
 4. transfer success -> transer.spec.js
-5. withdraw success -> withdraw.spec.js
-6. withdraw fail -> withdraw.spec.js
+5. transfer fail (account invalid) -> transer.spec.js
+6. transfer fail (amount invalid) -> transer.spec.js
+7. withdraw success -> withdraw.spec.js
+8. withdraw fail -> withdraw.spec.js
--- homework/transfer.spec.js
+++ homework/transfer.spec.js
@@ -33,3 +33,64 @@ test("Transfer success", async ({ page }) => {
   await expect(transferToast).toBeVisible({ timeout: 10000 });
   await expect(transferToast).toContainText("โอนเงินสำเร็จ");
 });
+
+test("Transfer fail (account invalid)", async ({ page }) => {
+  await page.goto("https://atm-buddy-lite.lovable.app/", {
+    waitUntil: "networkidle",
+  });
+
+  //Login with user 123456
+  await page.locator('input[type="text"]').fill("123456");
+  await page.locator('input[type="password"]').fill("1234");
+  await page.getByRole("button", { name: "เข้าสู่ระบบ" }).click();
+
+  const loginToast = page.locator('li[role="status"][data-state="open"]').first();
+  await expect(loginToast).toBeVisible({ timeout: 10000 });
+  await expect(loginToast).toContainText("เข้าสู่ระบบสำเร็จ");
+
+  //enter transfer page
+  await page.getByText("โอนเงิน", { exact: true }).click();
+
+  //input another account's number (non existed)
+  await page.getByPlaceholder("กรอกหมายเลขบัญชี 6 หลัก").fill("111111");
+  
+  //select the 2000 choice (not the textfield)
+  await page.getByRole("button", { name: "฿2,000.00" }).click();
+
+  //input note (not required)
+  await page.getByPlaceholder("เช่น เงินค่าอาหาร, ค่าเช่าบ้าน").fill("note");
+
+  //press the button
+  await page.getByRole("button", { name: "โอนเงิน" }).click();
+
+  const transferToast = page.locator('li[role="status"][data-state="open"]').last();
+  await expect(transferToast).toBeVisible({ timeout: 10000 });
+  await expect(transferToast).toContainText("ไม่พบบัญชีปลายทาง");
+});
+
+test("Transfer fail (amount invalid)", async ({ page }) => {
+  await page.goto("https://atm-buddy-lite.lovable.app/", {
+    waitUntil: "networkidle",
+  });
+
+  //Login with user 123456
+  await page.locator('input[type="text"]').fill("123456");
+  await page.locator('input[type="password"]').fill("1234");
+  await page.getByRole("button", { name: "เข้าสู่ระบบ" }).click();
+
+  const loginToast = page.locator('li[role="status"][data-state="open"]').first();
+  await expect(loginToast).toBeVisible({ timeout: 10000 });
+  await expect(loginToast).toContainText("เข้าสู่ระบบสำเร็จ");
+
+  //enter transfer page
+  await page.getByText("โอนเงิน", { exact: true }).click();
+
+  //input another account's number
+  await page.getByPlaceholder("กรอกหมายเลขบัญชี 6 หลัก").fill("789012");
+  
+  //Input invalid amount of money
+  await page.getByPlaceholder("0").fill("1000000");
+
+   const transferBtn = page.getByRole("button", { name: /โอนเงิน/ });
+  await expect(transferBtn).toBeDisabled();
+});
